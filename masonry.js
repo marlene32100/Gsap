ca86c60c205279741b8026747a19a93dba4d5cf3
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-04-29 13:06:48 +0200

    Fixed nav styling in portfolio

---
+++ masonry.js
@@ -0,0 +1,1 @@
+ScrollReveal().reveal(".masonry-img");
--- portfolio-app.js
+++ portfolio-app.js
@@ -10,7 +10,7 @@ if (window.screen.width >= 1024) {
     opacity: 0.4,
   })
     .fromTo(
-      ".nav-closed",
+      ".nav-closed-portfolio",
       0.5,
       {
         width: "20%",
@@ -25,11 +25,9 @@ if (window.screen.width >= 1024) {
       0.5,
       {
         x: 0,
-        color: "white",
       },
       {
         x: 100,
-        color: "#136356",
       },
       "-= 0.5"
     )
@@ -79,20 +77,98 @@ if (window.screen.width >= 1024) {
       },
       "-= 0.5"
     );
+} else if (window.screen.width >= 992) {
+  {
+    tl.to(".cover", 1, {
+      width: "60%",
+      ease: Power2.easeOut,
+      opacity: 0.4,
+    })
+      .fromTo(
+        ".nav-closed-portfolio",
+        0.5,
+        {
+          width: "20%",
+        },
+        {
+          width: "40%",
+        },
+        "-= 0.5"
+      )
+      .fromTo(
+        ".menu-icon",
+        0.5,
+        {
+          x: 0,
+        },
+        {
+          x: 100,
+        },
+        "-= 0.5"
+      )
+      .to(
+        "nav",
+        1,
+        {
+          height: "100%",
+          ease: Power2.easeOut,
+        },
+        "-= 0.5"
+      )
+      .fromTo(
+        ".nav-open",
+        0.5,
+        {
+          opacity: 0,
+          x: 50,
+          ease: Power2.easeOut,
+        },
+        {
+          opacity: 1,
+          x: 0,
+          onComplete: function () {
+            navOpen.style.pointerEvents = "auto";
+          },
+        }
+      )
+      .fromTo(
+        ".cover-date",
+        1,
+        {
+          opacity: 0.7,
+          fontSize: "2em",
+          left: "10%",
+          right: "auto",
+          bottom: "10%",
+          ease: Power2.easeOut,
+        },
+        {
+          opacity: 1,
+          fontSize: "5em",
+          left: "auto",
+          right: "1rem",
+          bottom: "50%",
+          onComplete: function () {
+            navOpen.style.pointerEvents = "auto";
+          },
+        },
+        "-= 0.5"
+      );
+  }
 } else {
   tl.to(".cover", 1, {
-    width: "60%",
+    width: "0%",
     ease: Power2.easeOut,
-    opacity: 0.4,
+    opacity: 0,
   })
     .fromTo(
-      ".nav-closed",
+      ".nav-closed-portfolio",
       0.5,
       {
         width: "20%",
       },
       {
-        width: "40%",
+        width: "100%",
       },
       "-= 0.5"
     )
@@ -101,11 +177,9 @@ if (window.screen.width >= 1024) {
       0.5,
       {
         x: 0,
-        color: "white",
       },
       {
         x: 100,
-        color: "#136356",
       },
       "-= 0.5"
     )
@@ -125,37 +199,16 @@ if (window.screen.width >= 1024) {
         opacity: 0,
         x: 50,
         ease: Power2.easeOut,
+        width: "40%",
       },
       {
         opacity: 1,
         x: 0,
+        width: "100%",
         onComplete: function () {
           navOpen.style.pointerEvents = "auto";
         },
       }
-    )
-    .fromTo(
-      ".cover-date",
-      1,
-      {
-        opacity: 0.7,
-        fontSize: "2em",
-        left: "10%",
-        right: "auto",
-        bottom: "10%",
-        ease: Power2.easeOut,
-      },
-      {
-        opacity: 1,
-        fontSize: "5em",
-        left: "auto",
-        right: "1rem",
-        bottom: "50%",
-        onComplete: function () {
-          navOpen.style.pointerEvents = "auto";
-        },
-      },
-      "-= 0.5"
     );
 }
 
